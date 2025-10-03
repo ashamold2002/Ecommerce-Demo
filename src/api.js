a36d59e42d7b08cdb3476bdc2815894afa25ef4d
asha <ashamold2002@gmail.com>
2025-10-04 03:12:31 +0400

    fourth commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE || 'http://localhost:5000/api'
+  baseURL: process.env.REACT_APP_API_BASE || 'REACT_APP_API_BASE=https://e-commerce-backend-1-k2vm.onrender.com/api'
 });
 
 export default API;
